b97af9d63428597059d4752ceea8103a2e55ce04
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-08-11 14:17:48 +0100

    ex2 done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,25 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Your code here # create an array with the same length as the tickets array
+    destinations = [None] * length
+    destination_lookup = dict()
 
-    return route
+    # store it's ticket's destination
+    for ticket in tickets:
+        destination_lookup[ticket.source] = ticket.destination
+        # print(f"destination lookup: {destination_lookup}") # the chain starts with NONE
+
+    # the starting ticket has a source of "NONE". Start there to build a chain
+    next_destination = destination_lookup['NONE']
+
+    for i in range(0, length):
+        # record next destination
+        destinations[i] = next_destination
+        print(f"destinations[i]: {destinations[i]}")
+
+        # retrieve next destination
+        next_destination = destination_lookup[next_destination]
+        print(f"next_destination: {next_destination}")
+
+    return destinations
